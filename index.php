24e37e7272bf6dcc0d2a204c471f49f8652c5372
NeilPrabhu <nprabhu@scu.edu>
2017-02-18 15:36:40 -0800

    Neil- version of home

--- index.php
+++ index.php
@@ -14,3 +14,38 @@
 
 
  */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EduCamps</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="">EduCamps</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="scheduleRegistration.php">Schedule/Registration</a></li>
+      <li><a href="#">Catalogue</a></li>
+      <li><a href="#">Forum</a></li>
+      <li><a href="#">Visuals</a></li>
+      <li><a href="#">More Info</a></li>
+    </ul>
+  </div>
+</nav>
+
+<div id = "welcome" class="container">
+  <h3>Welcome to EduCamps</h3>
+  <p>A place where youth strive for greatness.</p>
+</div>
+
+</body>
+</html>
