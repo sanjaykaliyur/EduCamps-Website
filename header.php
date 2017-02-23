e451fcd57c964ac0ff780fb6d34da1268dcfbd6c
NeilPrabhu <nprabhu@scu.edu>
2017-02-22 16:23:20 -0800

    Added to header
    Added to header, need help working on form validation between
    register.php and pay.php

--- header.php
+++ header.php
@@ -1,6 +1,30 @@
+<?php
 /* Header
 
   Site Logo
   Title
 
  */
+ ?>
+ <html lang="en">
+ <head>
+ <title>EduCamps</title>
+ <meta charset="utf-8">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <nav class="navbar navbar-default">
+   <div class="container-fluid">
+     <div class="navbar-header">
+       <a class="navbar-brand" href="">EduCamps</a>
+     </div>
+     <ul class="nav navbar-nav">
+       <li><a href="index.php">Home</a></li>
+       <li><a href="register.php">Schedule/Registration</a></li>
+       <li><a href="#">Catalogue</a></li>
+       <li><a href="#">Forum</a></li>
+       <li><a href="#">Visuals</a></li>
+       <li><a href="#">More Info</a></li>
+     </ul>
+   </div>
+ </nav>
--- pay.php
+++ pay.php
@@ -1,10 +1,12 @@
+<?php
+include 'header.php'
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>EduCamps</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <!--script type="text/javascript" src="https://ajax.microsoft.com/ajax/jQuery/jquery-1.4.2.min.js"></script-->
@@ -12,25 +14,8 @@
 
 </head>
 <body>
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.html">EduCamps</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="scheduleRegistration.html">Schedule/Registration</a></li>
-      <li><a href="#">Catalogue</a></li>
-      <li><a href="#">Forum</a></li>
-      <li><a href="#">Visuals</a></li>
-      <li><a href="#">More Info</a></li>
-    </ul>
-  </div>
-</nav>
 <?php
 
-// define variables and set to empty values
 $childName = $childDOB =$parentName = $parentName = $parentEmail = $childGrade = "";
 $campDuration = $special = "";
 
