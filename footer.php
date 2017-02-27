c37d31d5f5eb34a576fc7905e75e8ebfa432cd75
Sanjay Kaliyur <sanjay.kaliyur1225@gmail.com>
2017-02-26 18:18:54 -0800

    Conversion to PHP

---
+++ footer.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>EduCamps</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<style type='text/css'>
+#footer {
+  clear: both;
+  color: #666;
+  margin: 0 auto 10px auto;
+  width: 80%;
+  padding: 500px;
+  text-align: center;
+  background:black;
+  border-top: 1px solid #F2F2F2;
+}
+</style>
+
+<footer>
+    <div class="footer">
+        <div class="footer">
+            <p>Copyright &copy; Sanjay Kaliyur, Neil Prabhu, Alex Ripley, & Warren Tseng 2017</p>
+        </div>
+    </div>
+</footer>
