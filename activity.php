b42f1cb59b46bfc2e1a69d5dc9ba76b5bed4e491
Sanjay Kaliyur <sanjay.kaliyur1225@gmail.com>
2017-02-27 21:28:35 -0800

    temporary activity

--- activity.php
+++ activity.php
@@ -1,12 +1,54 @@
-<?php
-/* Activity
+<?php include 'header.php'; ?>
 
- */
+<html>
+  <head>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <link rel="stylesheet" href="TicTacToe/style.css" type="text/css">
+    <script src="TicTacToe/jquery.min.js"></script>
+    <script src="TicTacToe/sugar-1.2.5.min.js"></script>
+    <script type="text/javascript" src="TicTacToe/game.js"></script>
+  </head>
+  <body class="theme-01">
+    <h1>Tic Tac Toe</h1>
 
- include 'header.php';
+    <div id="game-container"></div>
+
+    <script type="text/x-template" id="game-template">
+    <div id="menu-container">
+
+      <ul id="menu" class="hidden">
+        <li id="time">Moves:</li>
+        <li id="status">Player X Wins!</li>
+        <li><a href="#restart" id="restart" class="button">RESTART</a></li>
+      </ul>
+    </div>
+
+    <table id="game" cellpadding="0" cellspacing="0">
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    </script>
+
+  </body>
+</html>
 
 
-
-
- include 'footer.php';
-?>
+<? include 'footer.php'; ?>
