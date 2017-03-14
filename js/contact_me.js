12985684ef56b7b19343e61e094f60d4c1bb5949
Sanjay Kaliyur <sanjay.kaliyur1225@gmail.com>
2017-03-13 19:21:44 -0700

    Cleaning up code

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,6 +1,6 @@
 /*
   Jquery Validation using jqBootstrapValidation
-   example is taken from jqBootstrapValidation docs 
+   example is taken from jqBootstrapValidation docs
   */
 $(function() {
 
@@ -23,7 +23,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./bin/contact_me.php",
+                url: "contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
